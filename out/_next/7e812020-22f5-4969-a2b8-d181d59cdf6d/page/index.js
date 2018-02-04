fb58fddd3acbc93780d47ad219bdd7bbbc8e4b26
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-02-04 00:12:41 -0500

    results

---
+++ out/_next/7e812020-22f5-4969-a2b8-d181d59cdf6d/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([5],{202:function(e,t,l){e.exports=l(203)},203:function(e,t,l){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var u=l(2),a=n(u),r=l(68),d=n(r),i=l(99),f=n(i),o=function(e){return a.default.createElement("li",null,a.default.createElement(f.default,{as:"/p/"+e.id,href:"/post?title="+e.title},a.default.createElement("a",null,e.title)))};t.default=function(){return a.default.createElement(d.default,null,a.default.createElement("h1",null,"My Blog"),a.default.createElement("ul",null,a.default.createElement(o,{id:"hello-nextjs",title:"Hello Next.js"}),a.default.createElement(o,{id:"learn-nextjs",title:"Learn Next.js is awesome"}),a.default.createElement(o,{id:"deploy-nextjs",title:"Deploy apps with Zeit"})))}}},[202]);
+            return { page: comp.default }
+          })
+        
---
+++ out/_next/7e812020-22f5-4969-a2b8-d181d59cdf6d/page/post.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/post', function() {
+            var comp = module.exports=webpackJsonp([4],{204:function(e,t,l){e.exports=l(205)},205:function(e,t,l){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=l(2),r=u(n),o=l(68),a=u(o);t.default=function(e){return r.default.createElement(a.default,null,r.default.createElement("h1",null,e.url.query.title),r.default.createElement("p",null,"This is the blog post content."))}}},[204]);
+            return { page: comp.default }
+          })
+        
